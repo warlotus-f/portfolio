5b7e164a85da484d34178afe958465676c0025d1
warlotus-f <abiodunonajobifortunatus@gmail.com>
2026-05-24 15:49:37 +0100

    first commit

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,20 @@
+import { Col } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, imgUrl }) => {
+  return (
+    <Col xs={12} sm={6} md={4}>
+      <div className="proj-imgbx">
+        <img
+          src={imgUrl}
+          alt={title}
+          className="img-fluid"
+        />
+
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+      </div>
+    </Col>
+  );
+};
