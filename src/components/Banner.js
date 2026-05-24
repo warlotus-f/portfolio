4db09a141d7db476e044ea4972828e11f8da93ae
warlotus-f <abiodunonajobifortunatus@gmail.com>
2026-05-24 21:44:21 +0100

    fix banner build errors

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,39 +10,41 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const toRotate = [ "Web Developer", "Web Designer", ];
   const period = 2000;
+  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
 
+  useEffect(() => {
+  let ticker = setInterval(() => {
+    tick();
+  }, delta);
 
-useEffect(() => {
+  return () => clearInterval(ticker);
+}, [text, delta, isDeleting, loopNum]);
+
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-
-    let updatedText = isDeleting
-      ? fullText.substring(0, text.length - 1)
-      : fullText.substring(0, text.length + 1);
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
     if (isDeleting) {
-      setDelta(prev => prev / 2);
+      setDelta(prevDelta => prevDelta / 2);
     }
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleting && updatedText === "") {
+    } else if (isDeleting && updatedText === '') {
       setIsDeleting(false);
-      setLoopNum(prev => prev + 1);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
       setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
     }
-  };
-
-  const ticker = setInterval(tick, delta);
-
-  return () => clearInterval(ticker);
-}, [text, delta, isDeleting, loopNum]);
+  }
 
   return (
     <section className="banner" id="home">
@@ -53,7 +55,7 @@ useEffect(() => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Fortunatus`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer",]'><span className="wrap">{text}</span></span></h1>
+                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,42 +1,88 @@
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
-export const Footer = () => {
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+
+  const period = 2000;
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+
+  useEffect(() => {
+    const tick = () => {
+      let i = loopNum % toRotate.length;
+      let fullText = toRotate[i];
+
+      let updatedText = isDeleting
+        ? fullText.substring(0, text.length - 1)
+        : fullText.substring(0, text.length + 1);
+
+      setText(updatedText);
+
+      if (isDeleting) {
+        setDelta(prev => prev / 2);
+      }
+
+      if (!isDeleting && updatedText === fullText) {
+        setIsDeleting(true);
+        setDelta(period);
+      } 
+      else if (isDeleting && updatedText === '') {
+        setIsDeleting(false);
+        setLoopNum(prev => prev + 1);
+        setDelta(500);
+      }
+    };
+
+    const ticker = setInterval(tick, delta);
+    return () => clearInterval(ticker);
+  }, [text, delta, isDeleting, loopNum]);
+
   return (
-    <footer className="footer">
+    <section className="banner" id="home">
       <Container>
-        <Row className="align-items-center">
-          <Col xs={12}>
-            <MailchimpForm />
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>
+                    {`Hi! I'm Judy `}
+                    <span className="txt-rotate">
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
+
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  </p>
+
+                  <button onClick={() => console.log('connect')}>
+                    Let’s Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              }
+            </TrackVisibility>
           </Col>
 
-          <Col xs={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
-
-          <Col xs={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="https://linkedin.com" target="_blank" rel="noreferrer">
-                <img src={navIcon1} alt="LinkedIn" />
-              </a>
-
-              <a href="https://github.com" target="_blank" rel="noreferrer">
-                <img src={navIcon2} alt="GitHub" />
-              </a>
-
-              <a href="https://twitter.com" target="_blank" rel="noreferrer">
-                <img src={navIcon3} alt="Twitter" />
-              </a>
-            </div>
-
-            <p>Copyright 2026. All Rights Reserved</p>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header" />
+                </div>
+              }
+            </TrackVisibility>
           </Col>
         </Row>
       </Container>
-    </footer>
+    </section>
   );
 };
